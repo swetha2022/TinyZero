4966110dfe4721d81d7ecf2a6d6a4e941cd30939
Swetha Rajkumar <rswetha330@gmail.com>
2025-07-16 15:59:23 -0700

    adding hindsight relabeling and gradient checkpointing

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -33,6 +33,55 @@ def _select_rm_score_fn(data_source):
     else:
         raise NotImplementedError
 
+class GCRewardManager(): 
+    """Goal conditioned reward manager.
+    """
+
+    def __init__(self, tokenizer, num_examine) -> None:
+        self.tokenizer = tokenizer
+        self.num_examine = num_examine  # the number of batches of decoded responses to print to the console
+
+    def __call__(self, data: DataProto):
+        """We will expand this function gradually based on the available datasets"""
+
+        reward_tensor = torch.zeros_like(data.batch['responses'], dtype=torch.float32)
+        already_print_data_sources = {}
+
+        for i in range(len(data)):
+            data_item = data[i]  # DataProtoItem
+
+            prompt_ids = data_item.batch['prompts']
+
+            prompt_length = prompt_ids.shape[-1]
+
+            valid_prompt_length = data_item.batch['attention_mask'][:prompt_length].sum()
+            valid_prompt_ids = prompt_ids[-valid_prompt_length:]
+
+            response_ids = data_item.batch['responses']
+            valid_response_length = data_item.batch['attention_mask'][prompt_length:].sum()
+            valid_response_ids = response_ids[:valid_response_length]
+
+            # decode
+            sequences = torch.cat((valid_prompt_ids, valid_response_ids))
+            sequences_str = self.tokenizer.decode(sequences)
+
+            ground_truth = data_item.non_tensor_batch['reward_model']['ground_truth']
+
+            data_source = data_item.non_tensor_batch['data_source']
+            compute_score_fn = _select_rm_score_fn(data_source)
+
+            # set format score to 0 for GC 1/0 reward (no partial rewards)
+            score = compute_score_fn(solution_str=sequences_str, ground_truth=ground_truth, format_score=0.)
+            reward_tensor[i, valid_response_length - 1] = score
+
+            if data_source not in already_print_data_sources:
+                already_print_data_sources[data_source] = 0
+
+            if already_print_data_sources[data_source] < self.num_examine:
+                already_print_data_sources[data_source] += 1
+                print(sequences_str)
+
+        return reward_tensor
 
 class RewardManager():
     """The reward manager.
